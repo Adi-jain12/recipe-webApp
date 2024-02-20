852ad401f0670c407b11fafa4e2850a3979bc6f8
aditya <jaditya98@gmail.com>
2024-02-21 01:06:42 +0530

    Refractoring into MVC structure

--- starter/src/js/model.js
+++ starter/src/js/model.js
@@ -1,3 +1,30 @@
+import { async } from 'regenerator-runtime';
+
 export const state = {
   recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  const res = await fetch(
+    'https://forkify-api.herokuapp.com/api/v2/recipes/${hashValue}'
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+  const { recipe } = data.data;
+
+  state.recipe = {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+  };
+
+  console.log(state.recipe);
 };
