6e16fe1b0fae17adc7b1080edf15d8af8befa35e
aditya <jaditya98@gmail.com>
2024-04-18 23:51:32 +0530

    add search functionality

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -35,3 +35,24 @@ const controlRecipes = async function () {
 
 // window.addEventListener('hashChange', controlRecipes);
 // window.addEventListener('load', controlRecipes);
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
